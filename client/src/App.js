38d75c72df68a868524c295694a3f22ac766d025
Oishi Yuikyo <s2008511@gmail.com>
2020-05-13 12:04:13 +0900

    Post and Comment feature

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,7 @@ import AddEducation from "./components/profile-form/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import Posts from "./components/posts/Posts";
+import Post from "./components/post/Post";
 
 if (localStorage.token) {
 	setAuthToken(localStorage.token);
@@ -85,6 +86,11 @@ const App = () => {
 								exact
 								path="/posts"
 								component={Posts}
+							/>
+							<PrivateRoute
+								exact
+								path="/post/:id"
+								component={Post}
 							/>
 						</Switch>
 					</section>
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,55 +3,88 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Moment from "moment";
 import { connect } from "react-redux";
+import { addLike, removeLike, deletePost } from "../../actions/post";
 
 const PostItem = ({
 	auth,
 	post: { _id, text, name, likes, user, avatar, comments, date },
+	addLike,
+	removeLike,
+	deletePost,
+	showActions,
 }) => {
 	return (
 		<div className="post bg-white p-1 my-1">
 			<div>
-				<a href="profile.html">
+				<Link to={`/profile/${user}`}>
 					<img className="round-img" src={avatar} alt="avatar" />
 					<h4>{name}</h4>
-				</a>
+				</Link>
 			</div>
 			<div>
 				<p className="my-1">{text}</p>
 				<p className="post-date">
-					Posted on
+					Posted on {date}
 					{/* <Moment format="YYYY/MM/DD">{date}</Moment> */}
 				</p>
-				<button type="button" className="btn btn-light">
-					<i className="fas fa-thumbs-up"></i>&ensp;
-					{likes.length > 0 && <span>{likes.length}</span>}
-				</button>
-				<button type="button" className="btn btn-light">
-					<i className="fas fa-thumbs-down"></i>
-				</button>
-				<Link to={`/post/${_id}`} className="btn btn-primary">
-					Discussion&ensp;
-					{comments.length > 0 && (
-						<span className="comment-count">{comments.length}</span>
-					)}
-				</Link>
-				{!auth.loading && user === auth.user._id && (
-					<button type="button" className="btn btn-danger">
-						<i className="fas fa-times"></i>
-					</button>
+
+				{showActions && (
+					<>
+						<button
+							type="button"
+							className="btn btn-light"
+							onClick={(e) => addLike(_id)}
+						>
+							<i className="fas fa-thumbs-up"></i>&ensp;
+							{likes.length > 0 && <span>{likes.length}</span>}
+						</button>
+						<button
+							type="button"
+							className="btn btn-light"
+							onClick={(e) => removeLike(_id)}
+						>
+							<i className="fas fa-thumbs-down"></i>
+						</button>
+						<Link to={`/post/${_id}`} className="btn btn-primary">
+							Discussion&ensp;
+							{comments.length > 0 && (
+								<span className="comment-count">
+									{comments.length}
+								</span>
+							)}
+						</Link>
+						{!auth.loading && user === auth.user._id && (
+							<button
+								type="button"
+								className="btn btn-danger"
+								onClick={(e) => deletePost(_id)}
+							>
+								<i className="fas fa-times"></i>
+							</button>
+						)}
+					</>
 				)}
 			</div>
 		</div>
 	);
 };
 
+PostItem.defaultProps = {
+	showActions: true,
+};
+
 PostItem.propTypes = {
 	post: PropTypes.object.isRequired,
 	auth: PropTypes.object.isRequired,
+	addLike: PropTypes.func.isRequired,
+	removeLike: PropTypes.func.isRequired,
+	deletePost: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
 	auth: state.auth,
 });
 
-export default connect(mapStateToProps, {})(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike, deletePost })(
+	PostItem,
+);
